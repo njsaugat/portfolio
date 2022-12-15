9f8ed73a21db5b5147766c3fe6de18cde3097723
Saugat Poudel <njsaugat@gmail.com>
2022-12-15 11:10:12 +0545

    tagline added

---
+++ portfolio/src/components/Tagline.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tagline = () => {
+  return (
+    <div>
+      <span className="magic text-white">Saugat Poudel</span>
+    </div>
+  );
+};
+
+export default Tagline;
