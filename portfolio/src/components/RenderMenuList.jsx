07432f60d8df4bb5159f7a3037c5efe225c88166
Saugat Poudel <njsaugat@gmail.com>
2022-12-16 10:15:05 +0545

    render menu list added

---
+++ portfolio/src/components/RenderMenuList.jsx
@@ -0,0 +1,64 @@
+import { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+// import { LoggedInContext } from '../App';
+// import ShowPortal from './ShowPortal';
+
+const RenderMenuList = ({ icons }) => {
+  const [showOptions, setShowOptions] = useState(false);
+  //   const { loggedIn } = useContext(LoggedInContext);
+
+  //   if (!loggedIn) {
+  //     return (
+  //       <div className="flex flex-col text-xl lg:w-full lg:ml-40 lg:flex-row gap-y-10 lg:gap-y-0 lg:gap-x-10 lg:text-lg">
+  //         <Link to={'/home'} rel="noreferrer" className="list-none">
+  //           {icons && icons[3]} Explore
+  //         </Link>
+  //         <Link to={'/login'} rel="noreferrer" className="list-none">
+  //           {icons && icons[1]} Log In
+  //         </Link>
+  //         <Link to={'/signup'} rel="noreferrer" className="list-none">
+  //           {icons && icons[2]} Sign Up
+  //         </Link>
+  //       </div>
+  //     );
+  //   }
+
+  return (
+    <>
+      <a to={'/home'} rel="noreferrer" className="list-none">
+        {icons && icons[0]} Home
+      </a>
+      <a to={'/movies-wishlist'} rel="noreferrer" className="list-none">
+        {icons && icons[1]} About Me
+      </a>
+      <a to={'/movies-wishlist'} rel="noreferrer" className="list-none">
+        {icons && icons[2]} Projects
+      </a>
+
+      <a to={'/home'} rel="noreferrer" className="list-none">
+        {icons && icons[3]} Contact Me
+      </a>
+      {/* <a href="# " rel="noreferrer" className="list-none">
+        {icons && icons[1]} About Us
+      </a> */}
+      {/* <div
+        className="list-none cursor-pointer"
+        onClick={(e) => {
+          setShowOptions(true);
+        }}
+      >
+        {icons && icons[1]} Log Out
+      </div> */}
+      {/* {showOptions && (
+        <ShowPortal
+          showOptions={showOptions}
+          setShowOptions={setShowOptions}
+          message={'Do you want to log out?'}
+          logOut={true}
+        />
+      )} */}
+    </>
+  );
+};
+
+export default RenderMenuList;
