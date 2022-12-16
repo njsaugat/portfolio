b001b034131e832510aa270cafbe27ddfafc05c2
Saugat Poudel <njsaugat@gmail.com>
2022-12-16 10:15:17 +0545

    tagline file added

--- portfolio/src/components/Tagline.jsx
+++ portfolio/src/components/Tagline.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Tagline = () => {
   return (
-    <div>
-      <span className="magic text-white">Saugat Poudel</span>
+    <div className="text-white text-5xl md:text-7xl leading-snug md:leading-normal ">
+      <span className="">
+        {' '}
+        Hi <span className="w-20 h-20 rotate-45 hi5 "> 👋</span>
+        <br />I am{' '}
+      </span>
+      <br />
+      <span className="magic text-white  font-bold ">Saugat Poudel</span>
     </div>
   );
 };
