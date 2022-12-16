c3b4205845a68e80136329b70826bb776dc18190
Saugat Poudel <njsaugat@gmail.com>
2022-12-16 10:13:51 +0545

    app file added

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,14 +5,20 @@ import Navbar from './components/Navbar';
 import Blobs from './components/Blobs';
 import Tagline from './components/Tagline';
 import Juggle from './components/Juggle';
+import Hero from './pages/Hero';
+import About from './pages/About';
+import Projects from './pages/Projects';
 
 function App() {
   return (
-    <div className="App bg-black w-screen h-screen text-5xl font-bold">
-      <Navbar />
-      <Blobs />
-      <Tagline />
-      <Juggle />
+    <div className="App bg-black w-screen h-screen  ">
+      {/* <Navbar /> */}
+      {/* <Blobs />
+      <Tagline /> */}
+      <Hero />
+      <About />
+      <Projects />
+      {/* <Juggle /> */}
     </div>
   );
 }
