1877f5e705707c6d3ef716635bac3921a10868a8
Saugat Poudel <njsaugat@gmail.com>
2022-12-17 07:42:00 +0545

    projects file added

--- portfolio/src/pages/Projects.jsx
+++ portfolio/src/pages/Projects.jsx
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const projectsList = [
   {
     id: 1,
     title: 'CineWish',
     link: 'https://github.com/njsaugat/moviesWishlist',
+    description:
+      'Cinewish is a one common ground for all your favorite movies. You can add movies to favorites, get movie recommendation, and even set email reminder to watch movies',
   },
   {
     id: 2,
     title: 'GharShar',
     link: 'https://github.com/njsaugat/houseMarket',
+    description:
+      'GharShar is a real estate app that allows users to either sale or rent their property. The user can list their properties, manage their profile, update the property listing. All basic CRUD operaions are supported',
   },
   {
     id: 3,
     title: '50_Projects',
     link: 'https://github.com/njsaugat/50_Projects__15_Days',
+    description:
+      'This is the random collection game that allows the users to catch the insect. It is a interesting pass time game developed.',
   },
-  {
-    id: 4,
-    title: 'Vanila Projects',
-    link: 'https://github.com/njsaugat/vanilaProjects',
-  },
+  // {
+  //   id: 4,
+  //   title: 'Vanila Projects',
+  //   link: 'https://github.com/njsaugat/vanilaProjects',
+  // },
 ];
 const Projects = () => {
+  useEffect(() => {
+    const contents = document.querySelectorAll('.project');
+    console.log(contents);
+    function showBox() {
+      const triggerBottom = (window.innerHeight / 5) * 4; //we are setting the trigger point; as we enter here event should fire up
+      contents.forEach((content) => {
+        const boxTop = content.getBoundingClientRect().top;
+        if (boxTop < triggerBottom) {
+          content.classList.add('show');
+        }
+      });
+    }
+    window.addEventListener('scroll', showBox);
+    return () => window.removeEventListener('scroll', showBox);
+  });
   const getRandom = () => Math.floor(Math.random() * 255);
   return (
     <div
@@ -30,31 +51,53 @@ const Projects = () => {
   from-slate-700 to-slate-900"
     >
       <h1 className="font-bold  text-5xl text-transparent drop-shadow-md bg-clip-text bg-gradient-to-t from-blue-50 to-blue-500   leading-tight md:leading-snug">
-        Projects
+        Featured Projects
       </h1>
 
-      <div className="projects flex flex-col md:flex-row items-center justify-center gap-y-5 gap-x-5  flex-wrap">
-        {projectsList.map((project) => {
+      <div className="projects flex flex-col  items-center justify-center gap-y-5 gap-x-5  ">
+        {projectsList.map((project, index) => {
           return (
-            <a
-              href={project.link}
-              target="_blank"
-              key={project.id}
-              className="rounded-3xl relative flex flex-col items-center shadow-slate-600 shadow-xl hover:shadow-2xl w-full md:w-1/2 lg:w-1/3 "
+            // <a
+            //   href={project.link}
+            //   target="_blank"
+            //   key={project.id}
+            //   className="rounded-3xl relative flex flex-col items-center shadow-slate-600 shadow-xl hover:shadow-2xl w-full md:w-1/2 lg:w-1/3 "
+            // >
+            //   {/* <img src={project}/> */}
+            //   <div className="w-10/12 overflow-hidden h-72  rounded-3xl my-4">
+            //     <img
+            //       src="https://picsum.photos/288"
+            //       alt=""
+            //       srcset=""
+            //       className=" rounded-3xl hover:scale-110 object-cover transition-all w-full h-full duration-500 ease-in-out"
+            //     />
+            //   </div>
+            //   <h2 className="text-xl my-3 font-bold">{project.title}</h2>
+            // </a>
+            <div
+              className={`${
+                index == 1 ? 'md:flex-row-reverse ' : ''
+              }flex flex-col md:flex-row items-center justify-center relative   w-full project`}
             >
-              {/* <img src={project}/> */}
-              <div className="w-10/12 overflow-hidden h-72  rounded-3xl my-4">
+              <div className="left w-full lg:w-1/2 md:w-full overflow-hidden h-96  rounded-3xl my-4">
                 <img
-                  src="https://picsum.photos/288"
+                  src="https://picsum.photos/1280"
                   alt=""
                   srcset=""
                   className=" rounded-3xl hover:scale-110 object-cover transition-all w-full h-full duration-500 ease-in-out"
                 />
               </div>
-              <h2 className="text-xl my-3 font-bold">{project.title}</h2>
-              {/* <span className="absolute w-20 h-20 rounded-full right-0 top-0 translate-x-1/2 -translate-y-1/2 bg-sky-300 "> */}{' '}
-              {/* </span> */}
-            </a>
+              <div
+                className={`right  md:h-auto -translate-y-1/3 opacity-70 md:opacity-100 md:-translate-x-10 ${
+                  index === 1 && 'md:translate-x-10'
+                } md:translate-y-0 rounded-3xl  flex flex-col items-center bg-black p-5  shadow-xl hover:shadow-2xl hover:shadow-slate-700 duration-300 w-full lg:w-1/3  `}
+              >
+                <h2 className="text-xl my-3 font-bold">{project.title}</h2>
+                <section className="text-slate-200">
+                  {project.description}
+                </section>
+              </div>
+            </div>
           );
         })}
       </div>
@@ -66,6 +109,7 @@ const Projects = () => {
       >
         {' '}
       </div>
+      {/* <span className="w-20 h-20 rounded-full right-0 top-0 translate-x-1/2 -translate-y-1/2 bg-sky-300 "></span> */}
     </div>
   );
 };
