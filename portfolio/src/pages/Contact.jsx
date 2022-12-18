9b347f1aaadb561780b701159d750b52e0021b0a
Saugat Poudel <njsaugat@gmail.com>
2022-12-18 11:22:36 +0545

    contact added

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -10,28 +10,35 @@ const Contact = () => {
   return (
     <div
       className="   w-screen h-screen lg:h-auto p-10 md:p-20 text-white bg-gradient-to-tr
-   from-slate-900 to-slate-900 flex items-center justify-center   gap-x-5 "
+   from-slate-900 to-slate-900 flex items-center justify-center  flex-col  gap-x-5 "
     >
+      <h1 className="font-bold self-start magic text-5xl text-transparent drop-shadow-md bg-clip-text bg-gradient-to-t from-blue-50 to-blue-500   leading-tight md:leading-snug">
+        Contact
+      </h1>
       <form className="contact-container w-11/12 md:w-10/12 lg:w-2/5 rounded-lg flex flex-col px-4 py-8 ">
-        <h3 className="text-3xl text-center mb-10">Contact</h3>
-        <div className="name field flex flex-col mb-20 relative">
+        <h3 className="text-3xl text-center mb-10">
+          Love to hear from you!
+          <span className="w-20 h-20 rotate-45 hi5 "> 👋</span>
+        </h3>
+        <div className="name field flex flex-col mb-20 text-lg relative">
           <input type="text" required />
           <label for="name">
             {createWave('Name')}
             {/* Email */}
           </label>
         </div>
-        <div className="email field field flex flex-col mb-20 relative">
+        <div className="email field field flex flex-col mb-20 text-lg relative">
           <input type="email" required />
           <label for="email">
             {createWave('Email')}
             {/* Email */}
           </label>
         </div>
-        <div className="message field field flex flex-col mb-20 relative">
+        <div className="message field field flex flex-col mb-20 text-lg  relative">
           <textarea
             type="text"
-            className="bg-transparent text-white shadow-sm shadow-slate-500"
+            className="bg-transparent h-36 resize-none text-white shadow-2xl shadow-slate-900"
+            // height="400"
             required
           ></textarea>
           <label for="message">
